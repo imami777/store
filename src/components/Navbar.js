053500838651e763949c0c90c7932cb3056c9b88
Ben Allen <cafe.e61@gmail.com>
2019-02-26 11:39:18 +0100

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import logo from "../logo.svg";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-small navbar-dark px-sm-5">
+        {/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */}
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+        </Link>
+        <ul className="navbar-nav.align-items-center">li</ul>
+      </nav>
+    );
+  }
+}
